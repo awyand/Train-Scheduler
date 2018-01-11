83315e0b386c756420ce7939a1c2a46d81a7d9dc
awyand <aaron.wyand@gmail.com>
2018-01-11 12:59:59 -0500

    add blinking arriving now text and basic styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
-  // Log current time in HH:mm
-  var currentTime = moment().format("HH:mm");
-  console.log(currentTime);
 
-  // Initialize Firebase
+  // Display current time on page
+  setInterval(function() {
+    $("#current-time").text(moment().format("HH:mm:ss"));
+  }, 1000);
+
+  ////// Firebase Initialization //////
+
   var config = {
     apiKey: "AIzaSyDuJehwSY6WYJWDaRzhPRsIXAVLcR4oK4E",
     authDomain: "moon-base-train-scheduler.firebaseapp.com",
@@ -20,54 +23,56 @@ $(document).ready(function() {
 
   ////// EVENT HANDLERS //////
 
+
   // Add train button click handler
   $(document).on("click", ".add-train-btn", function() {
     // Prevent default submit
     event.preventDefault();
 
+    // Save user input to variables
+    var userTrainName = $("#input-train-name").val().trim();
+    var userDestination = $("#input-destination").val().trim();
+    var userFirstTrainTime = $("#input-first-train-time").val().trim();
+    var userFrequency = $("#input-frequency").val().trim();
+
     // User input validation
+
     // Check to see if user left any fields blank
-    if (
-           $("#input-first-train-time").val().trim().length === 0
-        || $("#input-destination").val().trim().length === 0
-        || $("#input-first-train-time").val().trim().length === 0
-        || $("#input-frequency").val().trim().length === 0) {
-          // Tell the user to complete all fields
-          alert("please complete all fields");
+    if (userTrainName.length === 0 || userDestination.length === 0 || userFirstTrainTime.length === 0 || userFrequency.length === 0) {
+      // Tell the user to complete all fields
+      alert("please complete all fields");
+    // Check to see if user has entered invalid numbers for first train in HH:mm
+    // This should be handled by form input type (time) but as a fallback
+    } else if (parseInt(userFirstTrainTime.substring(0,2)) >= 24) {
+      // Tell the user that hours must be less than 24
+      alert("hours must be less than 24");
+    } else if (parseInt(userFirstTrainTime.substring(3,5)) >= 60) {
+      // Tell the user that minutes must be less than 60
+      alert("minutes must be less than 60");
+    // Check to see if user has entered invalid number for frequency
+    } else if (parseInt(userFrequency) <= 0) {
+      // Tell the user that frequnecy must be a positive number
+      alert("frequnecy must be a positive number");
+    // If all user input validation is okay, continue with Firebase population
+    } else {
+      // Save user inputs to a newTrain object to push to Firebase
+      var newTrain = {
+        trainName : $("#input-train-name").val().trim(),
+        destination : $("#input-destination").val().trim(),
+        firstTrainTime : $("#input-first-train-time").val().trim(),
+        frequency : $("#input-frequency").val().trim()
+      };
 
-        // Check to see if user has entered invalid numbers for first train in HH:mm
-        // This should be handled by form input type (time) but as a fallback
-        } else if (parseInt($("#input-first-train-time").val().trim().substring(0,2)) >= 24) {
-          // Tell the user that hours must be less than 24
-          alert("hours must be less than 24");
-        } else if ($("#input-first-train-time").val().trim().substring(3,5) >= 60) {
-          // Tell the user that minutes must be less than 60
-          alert("minutes must be less than 60");
+      // Upload to Firebase
+      db.ref().push(newTrain);
 
-        // Check to see if user has entered invalid number for frequency
-        } else if ($("#input-frequency").val().trim() <= 0) {
-          // Tell the user that frequnecy must be a positive number
-          alert("frequnecy must be a positive number");
-        // If all user input validation is okay, continue with Firebase population
-        } else {
-          // Save user inputs to a newTrain object to push to Firebase
-          var newTrain = {
-            trainName : $("#input-train-name").val().trim(),
-            destination : $("#input-destination").val().trim(),
-            firstTrainTime : $("#input-first-train-time").val().trim(),
-            frequency : $("#input-frequency").val().trim()
-          }
-
-          // Upload to Firebase
-          db.ref().push(newTrain);
-
-          // Clear Form
-          $("#input-train-name").val("");
-          $("#input-destination").val("");
-          $("#input-first-train-time").val("");
-          $("#input-frequency").val("");
-        }
-      });
+      // Clear Form
+      $("#input-train-name").val("");
+      $("#input-destination").val("");
+      $("#input-first-train-time").val("");
+      $("#input-frequency").val("");
+    }
+  });
 
   // Firebase event for child added hanlder
   db.ref().on("child_added", function(data, prevChildKey) {
@@ -79,9 +84,57 @@ $(document).ready(function() {
       dbFrequency : data.val().frequency
     }
 
-    // Calculate Next Arrival
 
-    // Calculate Minutes Away
+    // var testFirst = "01:00";
+    // var testFreq = "60";
+    // var testFirstCoverted = moment(testFirst,"HH:mm");
+    // var diffTime = moment().diff(moment(testFirstCoverted), "minutes");
+    //
+    // var remainder = diffTime % testFreq;
+    // var minutesRemaining = testFreq - remainder;
+    // console.log(`Minutes remaining: ${minutesRemaining}`);
+    // var nextTrain = moment().add(minutesRemaining, "minutes");
+    // var nextTrainConverted = moment(nextTrain).format("HH:mm");
+    // console.log(`Next Train: ${nextTrainConverted}`);
+
+    // Calculate Next Arrival and Minutes Away
+
+    // Initialize variables
+    var nextTrainTime = 0;
+    var minutesUntilNextTrain = 0;
+    var blinkStatus = "";
+
+    // Save frequency as a number for math purposes
+    var frequencyInt = parseInt(dbTrain.dbFrequency);
+    // Get first train time as a moment object in HH:mm
+    var dbFirstTrainTimeObject = moment(dbTrain.dbFirstTrainTime, "HH:mm");
+    // Calculate how many minutes have passed since first train
+    var minutesSinceFirstTrain = moment().diff(moment(dbFirstTrainTimeObject), "minutes");
+
+    // Check to see if first train is in the future
+    if (minutesSinceFirstTrain < 0) {
+      nextTrainTime = dbFirstTrainTimeObject;
+      minutesUntilNextTrain = Math.abs(minutesSinceFirstTrain);
+    } else {
+      // Divide minutes passed since first train by frequency and get remainder (i.e. how many minutes since the most recent train left)
+      var minutesSinceLastTrain = minutesSinceFirstTrain % frequencyInt;
+      // Get minutes remaining until next train
+      minutesUntilNextTrain = frequencyInt - minutesSinceLastTrain;
+      // Add minutes until next train to current time to get next train time
+      nextTrainTime = moment().add(minutesUntilNextTrain, "minutes");
+    }
+
+    // Convert next train time to HH:mm format
+    var nextTrainTimeConverted = moment(nextTrainTime).format("HH:mm");
+
+    // If 1 minutes remains until next train, style to alert the user that the train is arriving now
+    if (minutesUntilNextTrain === 1) {
+      minutesUntilNextTrain = "ARRIVING NOW";
+      blinkStatus = "blink";
+    } else {
+      blinkStatus = "";
+    }
+
 
     // Add each child to table
     $("#schedule-body").append(`
@@ -89,11 +142,9 @@ $(document).ready(function() {
         <td>${dbTrain.dbTrainName}</td>
         <td>${dbTrain.dbDestination}</td>
         <td>${dbTrain.dbFrequency}</td>
-        <td>Next Arrival</td>
-        <td>Minutes Away</td>
+        <td>${nextTrainTimeConverted}</td>
+        <td class="${blinkStatus}">${minutesUntilNextTrain}</td>
       </tr>
       `);
-
   });
-
 });
